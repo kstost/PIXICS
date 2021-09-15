c094d33f31a4d3a6124d7246eb421fcf52ac5bf0
kstost <monogatree@gmail.com>
2021-09-15 13:00:02 +0900

    fixed

--- PIXICS.js
+++ PIXICS.js
@@ -3,7 +3,9 @@ const PIXICS = (() => {
         constructor({ world }) {
             this.world = world;
             this.graphic = new PIXI.Graphics();
-            this.planckBody = world.createBody();
+            this.planckBody = world.createBody({
+                bullet: false,
+            });
             this.planckBody.setGravityScale(1);
             this.planckBody.setUserData(this);
             this.planckBody.setMassData({ mass: 1, center: planck.Vec2(0, 0), I: 1 });
@@ -163,6 +165,15 @@ const PIXICS = (() => {
             let { x, y } = this.getPosition();
             let gravityScale = this.planckBody.getGravityScale();
             let dynamic = this.planckBody.isDynamic();
+            let static_ = this.planckBody.isStatic();
+            let active = this.planckBody.isActive();
+            let awake = this.planckBody.isAwake();
+            let angularVelocity = this.planckBody.getAngularVelocity();
+            let linearDamping = this.planckBody.getLinearDamping();
+            let linearVelocity = this.planckBody.getLinearVelocity();
+            let bullet = this.planckBody.isBullet();
+            let kinematic = this.planckBody.isKinematic();
+            let fixedRotation = this.planckBody.isFixedRotation();
             let angle = this.getAngle();
             let fixtures = new Map();
             let fixtureList = [];//[...fixtures.keys()];
@@ -194,7 +205,9 @@ const PIXICS = (() => {
             this.planckBody.getWorld().destroyBody(this.planckBody);
 
             // 바디를 재생성 및 값 복원
-            this.planckBody = this.world.createBody();
+            this.planckBody = this.world.createBody({
+                bullet: false,
+            });
             this.planckBody.setGravityScale(gravityScale);
             this.planckBody.setUserData(this);
             this.planckBody.setMassData({ mass: 1, center: planck.Vec2(0, 0), I: 1 });
@@ -222,236 +235,44 @@ const PIXICS = (() => {
             // 다이나믹상태 복원
             dynamic && this.planckBody.setDynamic();
 
-            // 접해있던것 깨우기
-            contactList.forEach(contact => contact.setAwake(true));
-        }
-    }
-    class Shape extends PIXI.Graphics {
-        constructor({ world, x, y }) {
-            super();
-            this.world = world;
-            this.planckBody = world.createBody();
-            this.planckBody.setGravityScale(1);
-            this.planckBody.setUserData(this);
-            this.planckBody.setMassData({ mass: 1, center: planck.Vec2(0, 0), I: 1 });
-            this.setPosition(x, y);
-        }
-        syncState() {
-            this.rotation = -this.planckBody.getAngle();
-            let bodyPosition = this.planckBody.getPosition();
-            this.x = bodyPosition.x * PIXICS.worldscale;
-            this.y = -bodyPosition.y * PIXICS.worldscale;
-        }
-        setAngle(radian) {
-            this.planckBody.setAngle(radian);
-            this.syncState();
-            this.touchContacts();
-        }
-        getAngle() {
-            return this.planckBody.getAngle();
-        }
-        setPosition(x, y) {
-            if (x === undefined || y === undefined) return;
-            this.planckBody.setPosition(planck.Vec2(x / PIXICS.worldscale, -y / PIXICS.worldscale));
-            this.syncState();
-            this.touchContacts();
-        }
-        getPosition() {
-            let dd = this.planckBody.getPosition();
-            dd.x *= PIXICS.worldscale;
-            dd.y *= PIXICS.worldscale;
-            dd.y *= -1;
-            return dd;
-        }
-        touchContacts() {
-            for (let b = this.planckBody.getContactList(); b; b = b.next) {
-                b.contact.getFixtureA().getBody().setAwake(true);
-                b.contact.getFixtureB().getBody().setAwake(true);
-            }
-        }
-        getBody() {
-            return this.planckBody;
-        }
-        setDensity(value) {
-            let fixture = this.planckBody.getFixtureList();
-            fixture && fixture.setDensity(value);
-            this.resetBodyAndFixtures();
-        }
-        getDensity() {
-            let fixture = this.planckBody.getFixtureList();
-            return fixture && fixture.getDensity();
-        }
-        setFriction(value) {
-            let fixture = this.planckBody.getFixtureList();
-            fixture && fixture.setFriction(value);
-        }
-        getFriction() {
-            let fixture = this.planckBody.getFixtureList();
-            return fixture && fixture.getFriction();
-        }
-        setRestitution(value) {
-            let fixture = this.planckBody.getFixtureList();
-            fixture && fixture.setRestitution(value);
-        }
-        getRestitution() {
-            let fixture = this.planckBody.getFixtureList();
-            return fixture && fixture.getRestitution();
-        }
-        setDynamic() {
-            this.planckBody.setDynamic();
-        }
-        isDynamic() {
-            return this.planckBody.isDynamic();
-        }
-        resetBodyAndFixtures(_shape) {
-            /*
-                setDynamic() 을 하고난 뒤에 Fixture의 Shape을 바꾸거나 density를 바꾸고자 할때에는 아예 body를 제거하고 새롭게 재정의 해야한다
-                본 함수는 fixture가 하나일때에 대해서만 대응한다
-            */
-            let { x, y } = this.getPosition();
-            let gravityScale = this.planckBody.getGravityScale();
-            let dynamic = this.planckBody.isDynamic();
-            let angle = this.getAngle();
-            let fixture = this.planckBody.getFixtureList();
-            let shape = _shape ? _shape : fixture.getShape();
-            let restitution = fixture?.getRestitution();
-            let friction = fixture?.getFriction();
-            let density = fixture?.getDensity();
-            density = density ? density : 1;
-
-            // 접해있는것의 목록을 담기
-            let contactList = [];
-            for (let b = this.planckBody.getContactList(); b; b = b.next) {
-                let a = b.contact.getFixtureA().getBody();
-                let b = b.contact.getFixtureB().getBody();
-                contactList.push(a, b);
-            }
-
-            // 픽스쳐와 바디를 제거한다
-            fixture && this.planckBody.destroyFixture(fixture);
-            this.planckBody.getWorld().destroyBody(this.planckBody);
-
-            // 바디를 재생성 및 값 복원
-            this.planckBody = this.world.createBody();
-            this.planckBody.setGravityScale(gravityScale);
-            this.planckBody.setUserData(this);
-            this.planckBody.setMassData({ mass: 1, center: planck.Vec2(0, 0), I: 1 });
-
-            // 픽스쳐 재생성 및 값 복원
-            fixture = this.planckBody.createFixture(shape);
-            fixture.setRestitution(restitution || 0);
-            fixture.setFriction(friction || 0);
-            fixture.setDensity(density || 0);
-
-            // 바디 상태값 재조정
-            this.setPosition(x, y);
-            this.setAngle(angle);
-
-            // 다이나믹상태 복원
-            dynamic && this.planckBody.setDynamic();
+            // 그외 바디의 상태값 복원
+            kinematic && this.planckBody.setKinematic(); // https://piqnt.com/planck.js/BodyTypes
+            static_ && this.planckBody.setStatic();
+            this.planckBody.setBullet(bullet);
+            this.planckBody.setAwake(awake);
+            this.planckBody.setActive(active);
+            this.planckBody.setFixedRotation(fixedRotation);
+            this.planckBody.setAngularVelocity(angularVelocity);
+            this.planckBody.setLinearDamping(linearDamping);
+            this.planckBody.setLinearVelocity(linearVelocity);
 
             // 접해있던것 깨우기
             contactList.forEach(contact => contact.setAwake(true));
         }
     }
-    class Rect extends Shape {
-        constructor({ world, width, height, x, y }) {
-            super({ world, x, y });
-            this.setSize(width, height);
+    //------------------------------
+    // 매 프레임마다 물리연산을 해서 나오는 수치를 픽시 그래픽요소의 상태에 반영
+    let updateList = [];
+    PIXI.Ticker.shared.add(dt => {
+        world.step(((1 * dt) / 60));
+        world.clearForces();
+        for (let body = world.getBodyList(); body; body = body.getNext()) {
+            body.getUserData()?.syncState();
         }
-        setSize(width, height) {
-            if (width === undefined || height === undefined) return;
-            this.clear();
-            this.beginFill(0xffffff);
-            this.drawRect(0, 0, width, height);
-            this.endFill();
-            this.pivot.set(this.width / 2, this.height / 2)
-            if (this.planckBody) {
-                let shape = planck.Box(this.width / 2 / PIXICS.worldscale, this.height / 2 / PIXICS.worldscale);
-                this.resetBodyAndFixtures(shape);
-            }
-        }
-        addRevoluteJoint(bar, x, y, pivotx, pivoty) {
-            //-----------------------
-            let fix = this;
-            let newone_width = bar.width;
-            let newone_height = bar.height;
-            let barx = -(fix.width / 2);
-            let bary = -(fix.height / 2);
-            //----------
-            barx += x; // fix상에서의 x
-            bary += y; // fix상에서의 y
-            let addx = pivotx; // new의 pivot point
-            let addy = pivoty; // new의 pivot point
-            //-----------------------
-            let extx = 0;
-            let exty = 0;
-            //-----------------------
-            bar.setSize(newone_width, newone_height);
-            bar.setPosition(barx + addx, bary + addy);
-            //-----------------------
-            1 && world.createJoint(planck.RevoluteJoint({
-                collideConnected: false,
-                // motorSpeed: 1 * Math.PI,
-                // maxMotorTorque: 0,
-                // enableMotor: true,
-            }, fix.getBody(), bar.getBody(), planck.Vec2(
-                (barx + extx) / PIXICS.worldscale,
-                -(bary + exty) / PIXICS.worldscale)
-            ));
-            //-----------------------
+        updateList.forEach(u => u(dt));
+    });
 
+    let point = {
+        worldscale: 0, PhysicsGraphics,
+        transScale(v) { return v / PIXICS.worldscale; },
+        createWorld(scale, gravity) {
+            point.worldscale = scale;
+            let world = new planck.World(gravity);
+            return {
+                world,
+                update: function (cb) { updateList.push(cb); }
+            };
         }
-    }
-    class Circle extends Shape {
-        constructor({ world, radius, x, y }) {
-            super({ world, x, y });
-            this.setRadius(radius);
-        }
-        setRadius(radius) {
-            // if (width === undefined || height === undefined) return;
-            this.clear();
-            this.beginFill(0xffffff);
-            this.drawCircle(0, 0, radius / 2);
-            this.endFill();
-            if (this.planckBody) {
-                let shape = planck.Circle(radius / 2 / PIXICS.worldscale, radius / 2 / PIXICS.worldscale);
-                this.resetBodyAndFixtures(shape);
-            }
-        }
-    }
-    class Polygon extends Shape {
-        constructor({ world, path, x, y }) {
-            super({ world, x, y });
-            this.setPath(path);
-        }
-        setPath(path) {
-            if (path === undefined) return;
-            this.clear();
-            this.beginFill(0xffffff);
-            for (let i = 0; i < path.length; i++) {
-                if (i === 0) {
-                    this.moveTo((path[i].x), (path[i].y));
-                } else {
-                    this.lineTo((path[i].x), (path[i].y));
-                }
-            }
-            this.closePath();
-            this.endFill();
-            this.pivot.set(0, 0)
-            if (this.planckBody) {
-                let shape = planck.Polygon(path.map(point => {
-                    point.x /= PIXICS.worldscale;
-                    point.y /= PIXICS.worldscale;
-                    point.y *= -1
-                    return point;
-                }));
-                this.resetBodyAndFixtures(shape);
-            }
-        }
-    }
-    return {
-        worldscale: 20, Rect, Circle, Polygon, PhysicsGraphics,
-        transScale(v) { return v / PIXICS.worldscale; }
     };
+    return point;
 })();
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 //------------------------------
 // 픽시스크린의 준비
 let display = displaySystem(1080, 1920, true);
@@ -16,23 +15,17 @@ app.stage.sortableChildren = true;
 
 //------------------------------
 // 피직스월드의 준비
-let gravity = planck.Vec2(0, -10);
-let world = new planck.World(gravity);
-
-//------------------------------
-// 매 프레임마다 물리연산을 해서 나오는 수치를 픽시 그래픽요소의 상태에 반영
-PIXI.Ticker.shared.add(dt => {
-    world.step(((1 * dt) / 60));
-    world.clearForces();
-    for (let body = world.getBodyList(); body; body = body.getNext()) {
-        body.getUserData()?.syncState();
-    }
-});
+const scale = 100;
+const gravity = planck.Vec2(0, -10);
+const pixics = PIXICS.createWorld(scale, gravity);;
+const world = pixics.world;
+pixics.update(function (dt) { /*매프레임마다 수행시킬코드*/ });
 
 //------------------------------
 // 화면 사방을 막아주는 바운더리 요소를 만들어서 화면에 등장시키기
 let 선굵기 = display.tr(10);
 let 바운더리 = new PIXICS.PhysicsGraphics({ world });
+바운더리.getBody().setBullet(true); // 컨티너스컬리젼디텍션 켜기
 바운더리.drawRect(0, 0, display.tr(display.width), 선굵기);
 바운더리.drawRect(0, display.tr(display.height) - 선굵기, display.tr(display.width), 선굵기);
 바운더리.drawRect(0, 0, 선굵기, display.tr(display.height));
@@ -41,10 +34,10 @@ let 바운더리 = new PIXICS.PhysicsGraphics({ world });
 app.stage.addChild(바운더리.getGraphic());
 
 //------------------------------
+// 그래픽요소에 여러가지 도형을 그려서 합쳐버릴수 있다
 let 여러가지조합 = new PIXICS.PhysicsGraphics({ world });
 여러가지조합.getGraphic().tint = 0x00aaff; // 색 지정이 가능. 그래픽요소의 기본색은 흰색
 여러가지조합.setDynamic(); // 중력법칙 적용
-// pg그래픽요소에 여러가지 도형을 그려서 합쳐버릴수 있다
 여러가지조합.drawPolygon([ // 다각형그리기
     planck.Vec2(display.tr(0), display.tr(0)),
     planck.Vec2(display.tr(30), display.tr(70)),
@@ -54,10 +47,26 @@ let 여러가지조합 = new PIXICS.PhysicsGraphics({ world });
 ]);
 여러가지조합.drawCircle(display.tr(210), display.tr(110), display.tr(50)); // 원그리기
 여러가지조합.drawRect(display.tr(250), display.tr(110), display.tr(50), display.tr(50)); // 사각형 그리기
-여러가지조합.setDensity(1000, 2); // 세번째 요소인 Rect의 밀도를 1000으로 지정 (기본은 1)
+여러가지조합.setDensity(1, 2); // 세번째 요소인 Rect의 밀도를 1으로 지정 (기본은 1)
 console.log(여러가지조합.getDensity(2)); // 밀도 확인 가능
 app.stage.addChild(여러가지조합.getGraphic()); // 그래픽요소를 화면에 추가
 setDraggable(여러가지조합); // 그래그 이벤트 붙이기
+
+//------------------------------
+// 돌아가는 막대 만들기
+let 돌아가는막대 = new PIXICS.PhysicsGraphics({ world });
+돌아가는막대.getGraphic().tint = 0x00aaff; // 색 지정이 가능. 그래픽요소의 기본색은 흰색
+돌아가는막대.setDensity(1110)
+돌아가는막대.drawRect(display.tr(-2080 / 2), display.tr(-5), display.tr(2080), display.tr(10)); // 사각형 그리기. 사각형을 그려주는 위치는 회전축과 연관이 있게된다
+돌아가는막대.setPosition(display.tr(1080 / 2), display.tr(1920 / 2))
+app.stage.addChild(돌아가는막대.getGraphic()); // 그래픽요소를 화면에 추가
+돌아가는막대.getBody().setBullet(true); // 막대는 얇고 얇은만큼 이 옵션을 안켜주면 충돌체가 투과해버릴 수 있다
+돌아가는막대.getBody().setKinematic(); // 다이나믹과 다르게 중력에 영향을 받지 않으며 Velocity 값이 영속적이게 된다
+돌아가는막대.getBody().setAngularVelocity(1); // 자율적이고 영속적인 회전힘을 부여
+돌아가는막대.getGraphic().interactive = true;
+돌아가는막대.getGraphic().on('mousedown', e => {
+    돌아가는막대.getBody().setLinearVelocity(planck.Vec2(0, 0));
+});
 
 //------------------------------
 // 동그라미들 만들어서 추가
@@ -66,7 +75,7 @@ for (let i = 0; i < 10; i++) {
     pg.drawCircle( // 원을 그림
         display.actualWidth * Math.random(), // 중심점의 X좌표
         display.actualHeight * Math.random(), // 중심점의 Y좌표
-        display.tr((display.width - 선굵기 * 2) / 20) // 반지름
+        display.tr((display.width - 선굵기 * 2) / 10) // 반지름
     );
     pg.setDynamic(); // 물리법칙의 영향을 받도록 해줌
     pg.setRestitution(0); // 탄성. 수치가 낮으면 안튕김, 수치가 높으면 튕김
