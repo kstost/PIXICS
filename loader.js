1be19bd8199e9319e4ea9a217a2b32cd77ac9e2a
kst <kst@ksts-Mac-mini.local>
2022-01-04 23:26:18 +0900

    fixed

--- loader.js
+++ loader.js
@@ -16,7 +16,7 @@ async function initPixics(initValue) {
         });
     }
     if (false) {
-        const { app, display, pixics } = await initPixics({
+        const { app, pixics, world, ratio, width, height } = await initPixics({
             resolution: { width: 1080, height: 1920 },
             fpsmonitor: true,
             container: document.querySelector('body'),
@@ -27,19 +27,20 @@ async function initPixics(initValue) {
     if (initValue) {
 
         // 스크린의 준비
-        const display = displaySystem(initValue.width, initValue.height, !!initValue.fpsmonitor); // 스크린너비, 스크린높이, 성능측정모니터사용여부
+        const display = displaySystem(initValue.resolution.width, initValue.resolution.height, !!initValue.fpsmonitor); // 스크린너비, 스크린높이, 성능측정모니터사용여부
         const app = display.createPIXIApp(); // 화면을 생성한다 PIXI Application Stage 를 생성한다
-        const { ratio, width, height } = display; // 계산된 실제 화면크기를 얻는다. width/ratio 한 값은 displaySystem 함수 첫번째인자로 넣은 숫자와 같다.
         initValue.container.appendChild(app.view);
+        const { ratio, width, height } = display; // 계산된 실제 화면크기를 얻는다. width/ratio 한 값은 displaySystem 함수 첫번째인자로 넣은 숫자와 같다.
 
         // 피직스월드의 준비
-        const pixics = PIXICS.createWorld(initValue.worldscale, ratio, initValue.gravity, true, display); // 첫번째 인자의 숫자는 커질수록 요소의 움직임이 빨라진다. 빨라지는 이유는 실제 화면상 픽셀수와 피직스월드의 수치와의 스케일을 나타내는 값이기 때문이다.
+        const pixics = PIXICS.createWorld(initValue.worldscale, display.ratio, initValue.gravity, true, display); // 첫번째 인자의 숫자는 커질수록 요소의 움직임이 빨라진다. 빨라지는 이유는 실제 화면상 픽셀수와 피직스월드의 수치와의 스케일을 나타내는 값이기 때문이다.
         const world = pixics.world;
         return {
             app,
             pixics,
             world,
-            display: { ratio, width, height },
+            display,
+            ...{ ratio, width, height }
         }
     }
 }
