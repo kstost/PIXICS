bf6567faf14aff2ac92e0ed8466cedd009977894
kst <kst@ksts-Mac-mini.local>
2022-01-05 04:51:54 +0900

    fixed

--- main.js
+++ main.js
@@ -1,56 +1,18 @@
 window.addEventListener('load', async () => {
 
-
-   function setEvent(thing, names, fn) {
-      thing.interactive = true;
-      [names].flat(Infinity).forEach(name => thing.on(name, fn));
-   }
-   function setDragable(thing) {
-      let startpoint = null;
-      let base = null;
-      let dotposes = null;
-      let dn = false;
-      const onDragStart = function (event) {
-         this.data = event.data; this.dragging = true;
-         const newPosition = this.data.getLocalPosition(this.parent);
-         startpoint = (newPosition);
-         base = { x: 0, y: 0 };
-         dotposes = { x: thing.x, y: thing.y };
-         dn = thing.isDynamic();
-         thing.setStatic()
-      }
-      const onDragEnd = function () {
-         this.dragging = false; this.data = null;
-         startpoint = null;
-         base = null;
-         if (dn) thing.setDynamic()
-      }
-      const onDragMove = function (e) {
-         if (this.dragging) {
-            const newPosition = this.data.getLocalPosition(this.parent);
-            let cha = {
-               x: (newPosition.x - startpoint.x),
-               y: (newPosition.y - startpoint.y),
-            };
-            base.x += cha.x;
-            base.y += cha.y;
-            let cp = thing.getPosition();
-            cp.x += cha.x;
-            cp.y += -cha.y;
-            thing.setPosition(cp.x, cp.y);
-            startpoint = (newPosition);
-         }
-      }
-      setEvent(thing.getGraphic(), ['mousedown', 'touchstart'], onDragStart);
-      setEvent(thing.getGraphic(), ['mousemove', 'touchmove'], onDragMove);
-      setEvent(thing.getGraphic(), ['mouseup', 'mouseupoutside', 'touchend', 'touchendoutside'], onDragEnd);
-   }
+   // const { app, pixics, world, size, PIXICS, b2 } = await initPixics({
+   //     resolution: { width: 100, height: 130 },
+   //     fpsmonitor: true,
+   //     container: document.querySelector('.container'),
+   //     gravity: { x: 0, y: -10 },
+   //     worldscale: 7,
+   // });
    //------------------------------
    // 스크린의 준비
-   const display = displaySystem(1080, 1920, true); // 스크린너비, 스크린높이, 성능측정모니터사용여부
+   const display = displaySystem(200, 200, true, document.querySelector('.container')); // 스크린너비, 스크린높이, 성능측정모니터사용여부
    const app = display.createPIXIApp(); // 화면을 생성한다 PIXI Application Stage 를 생성한다
    const { ratio, width, height } = display; // 계산된 실제 화면크기를 얻는다. width/ratio 한 값은 displaySystem 함수 첫번째인자로 넣은 숫자와 같다.
-   document.querySelector('body').appendChild(app.view);
+   display.container.appendChild(app.view);
 
    //------------------------------
    // 피직스월드의 준비
@@ -60,69 +22,8 @@ window.addEventListener('load', async () => {
    const L = pixics.log;
 
 
-   //
-
-   //------------------------------
-   L('메인 코드의 시작');
-   await pixics.sleep(10);
-
-   L('바운더리 생성');
-   let tickness = 2 * ratio;
-   let boundary = new PIXICS.PhysicsGraphics({ world });
-   boundary.getBody().SetBullet(true);
-   boundary.drawRect(width * 0.7 / 2, 0, tickness, width * 0.35);
-   boundary.drawRect(-width * 0.7 / 2, 0, tickness, width * 0.35);
-   boundary.drawRect(0, width * 0.7 / 2, width * 0.35, tickness);
-   boundary.drawRect(0, -width * 0.7 / 2, width * 0.35, tickness);
-   (async () => {
-      while (true) await boundary.rotateEaseBy(Math.PI / 8, 4000);
-   })();
-   app.stage.addChild(boundary.getGraphic());
-   await pixics.sleep(500);
-
-   let list = [];
-   for (let i = 0; i < 40; i++) {
-      L(`공${i+1} 생성`);
-      !(i % 1) && await pixics.sleep(1);
-      let cir = new PIXICS.PhysicsGraphics({ world });
-      cir.drawCircle(0 * ratio, 0 * ratio, (50) * ratio)
-      cir.setDynamic()
-      cir.addIgnoreContact(boundary);
-      cir.setGravityScale(1)
-      cir.setPosition((0.5 - Math.random()) * width * 0.5, (0.5 - Math.random()) * width * 0.5)
-      cir.getBody().SetLinearVelocity(new b2.Vec2(1 * ratio * (0.5 - Math.random()), 1 * ratio * (0.5 - Math.random())));
-      app.stage.addChild(cir.getGraphic());
-      cir.getGraphic().interactive = true;
-      setEvent(cir.getGraphic(), ['mousedown', 'touchstart'], e => {
-         cir.destroy();
-      });
-      let text = new PIXI.Text('', { fontFamily: 'Arial', fontSize: 70 * ratio, fill: 0x000000, align: 'center' });
-      cir.getGraphic().addChild(text);
-      function setLabel(val) {
-         text.text = val;
-         text.x = -(text.width / 2)
-         text.y = -(text.height / 2)
-      }
-      cir.setRestitution(0);
-      cir.setTag(i);
-      list.push(cir);
-      pixics.update(function () {
-         let vd = cir;
-         vd.getGraphic().tint = vd.getContactCount() ? 0xaa77ff : 0xffffff;
-         setLabel(vd.getContactCount());
-      })
-   }
-   for (let i = 0; 1 && i < list.length; i++) {
-      for (let j = 0; j < list.length; j++) {
-         let vd = list[i];
-         let vx = list[j];
-         vd.addEvent('contact', vx, function (vx) {
-            // console.log('on_', vd.getTag())
-         });
-         vd.addEvent('untact', vx, function (vx) {
-            // console.log('off', vd.getTag())
-         });
-      }
-   }
+   // let box = new PIXICS.PhysicsGraphics({ world });
+   // box.drawRect(0, 0, 100 * size.ratio / 4, size.actualSize.width / 4);
+   // app.stage.addChild(box.getGraphic());
 
 });
--- system.js
+++ system.js
@@ -1,4 +1,4 @@
-let displaySystem = (width, height, fps) => {
+let displaySystem = (width, height, fps, container) => {
     if (fps) {
         (function () { var script = document.createElement('script'); script.onload = function () { var stats = new Stats(); document.body.appendChild(stats.dom); requestAnimationFrame(function loop() { stats.update(); requestAnimationFrame(loop) }); }; script.src = '//mrdoob.github.io/stats.js/build/stats.min.js'; document.head.appendChild(script); })();
     }
@@ -22,28 +22,12 @@ let displaySystem = (width, height, fps) => {
             return app;
         },
         log(v) {
-            // document.createElement();
-            return;
-            // #log {
-            //     position: fixed;
-            //     z-index: 100;
-            //     top:0px;
-            //     left:0px;
-            //     color:white;
-            //     text-align: left;
-            //     /* display: none; */
-            // }
-            if (!this.log.count) { this.log.count = 0; }
-            this.log.count++;
-            let el = document.querySelector('#log');
-            if (el) {
-                el.innerHTML += `${this.log.count} - ${v}<br />`
-            }
         },
         getRatio() {
-            let ratio = window.innerHeight / original.height;
-            if (original.width * ratio > window.innerWidth) {
-                ratio = window.innerWidth / original.width;
+            const { width, height } = container.getBoundingClientRect();
+            let ratio = height / original.height;
+            if (original.width * ratio > width) {
+                ratio = width / original.width;
             }
             return ratio;
         },
@@ -51,37 +35,6 @@ let displaySystem = (width, height, fps) => {
     pointer.ratio = pointer.getRatio();
     pointer.width = pointer.ratio * width;
     pointer.height = pointer.ratio * height;
+    pointer.container = container;
     return pointer;
-
-    return;
-    if (fps) {
-        (function () { var script = document.createElement('script'); script.onload = function () { var stats = new Stats(); document.body.appendChild(stats.dom); requestAnimationFrame(function loop() { stats.update(); requestAnimationFrame(loop) }); }; script.src = '//mrdoob.github.io/stats.js/build/stats.min.js'; document.head.appendChild(script); })();
-    }
-    let display = {
-        width,
-        height,
-        tr(v) { return v * (display.actualWidth / display.width); },
-        rt(v) { return v * (display.width / display.actualWidth); },
-        // log(v) {
-        //     if (!log.count) { log.count = 0; }
-        //     log.count++;
-        //     let el = document.querySelector('#log');
-        //     if (el) {
-        //         el.innerHTML += `${log.count} - ${v}<br />`
-        //     }
-        // },
-        sizeCalc() {
-            let _width = window.innerWidth;
-            let _height = (display.height / display.width) * window.innerWidth;
-            if (window.innerHeight <= _height) {
-                _height = window.innerHeight;
-                _width = (display.width / display.height) * window.innerHeight;
-            }
-            if (window.innerHeight > _height) {
-                document.body.style.marginTop = `${(window.innerHeight - _height) * 0.5}px`
-            }
-            return { width: _width, height: _height };
-        }
-    }
-    return display;
 }
