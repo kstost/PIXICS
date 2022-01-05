6f667a944aa3aa6512e53237577632863812868d
kst <kst@ksts-Mac-mini.local>
2022-01-05 20:14:32 +0900

    fixed

--- loader.js
+++ loader.js
@@ -2,8 +2,8 @@ async function initPixics(initValue) {
     let scriptlist = [
         "https://pixijs.download/v6.2.1/pixi.min.js",
         "https://cdn.jsdelivr.net/gh/kstost/ksttool/ksttool.js",
-        "https://cdn.jsdelivr.net/gh/kstost/PIXICS@59eb50b/PIXICS.js",
-        "https://cdn.jsdelivr.net/gh/kstost/PIXICS@59eb50b/system.js",
+        "https://cdn.jsdelivr.net/gh/kstost/PIXICS@b47091c/PIXICS.js",
+        "https://cdn.jsdelivr.net/gh/kstost/PIXICS@b47091c/system.js",
         "https://cdn.jsdelivr.net/gh/flyover/box2d.ts@4bea859e7b1bab55429d76e03f72b1de72edc5f8/dist/box2d.umd.js",
         // "https://rawcdn.githack.com/flyover/box2d.ts/master/dist/box2d.umd.js",
     ];
