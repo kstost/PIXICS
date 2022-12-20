59e3d1a6df7233a4d14491de1bea73aa4fadd945
kst <kst@ksts-Mac-mini.local>
2022-12-21 05:50:34 +0900

    fixed

--- src/pixics.src.js
+++ src/pixics.src.js
@@ -438,6 +438,38 @@ const pixiInst = function () {
                 this.rotation = Math.atan2(this.by - this.ay, this.bx - this.ax);
             }
         }
+        class TilingLine extends PIXI.TilingSprite {
+            spec = {}
+            constructor(image) {
+                super(image);
+                this.anchor.y = 0.5;
+                this.axis = 0.5;
+                this.thickness = 1;
+            }
+            set thickness(v) { this.height = v; }
+            get thickness() { return this.height; }
+            set axis(v) { this.anchor.x = v; }
+            get axis() { return this.anchor.x; }
+            set ax(v) { this.spec.ax = v; this.calcpos(); }
+            get ax() { return this.spec.ax ?? 0; }
+            set ay(v) { this.spec.ay = v; this.calcpos(); }
+            get ay() { return this.spec.ay ?? 0; }
+            set bx(v) { this.spec.bx = v; this.calcpos(); }
+            get bx() { return this.spec.bx ?? 0; }
+            set by(v) { this.spec.by = v; this.calcpos(); }
+            get by() { return this.spec.by ?? 0; }
+            get length() {
+                let a = this.ax - this.bx;
+                let b = this.ay - this.by;
+                return Math.sqrt(a * a + b * b);
+            }
+            calcpos() {
+                this.width = this.length;
+                this.x = this.bx - ((this.bx - this.ax) * (1 - this.anchor.x));
+                this.y = this.by - ((this.by - this.ay) * (1 - this.anchor.x));
+                this.rotation = Math.atan2(this.by - this.ay, this.bx - this.ax);
+            }
+        }
         class ObjectPool {
             pool = new Map();
             static resting = Symbol.for('resting');
@@ -2219,6 +2251,7 @@ const pixiInst = function () {
             Image,
             ObjectPool,
             Line,
+            TilingLine,
             editorUrl(json, redirect) {
                 let data = encodeURIComponent(JSON.stringify(json));
                 let a = document.createElement('a');
